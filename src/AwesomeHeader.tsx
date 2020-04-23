d809edfec60aaf7d313984112a870dbddbef18ec
Marc Lammers <marc@lammers.dev>
2020-04-23 21:40:42 +0200

    AwesomeHeader added.

--- src/AwesomeHeader.tsx
+++ src/AwesomeHeader.tsx
@@ -1,12 +1,73 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Platform } from 'react-native'
 import React from 'react'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 
-const AwesomeHeader = () => {
+interface AwesomeHeaderProps {
+  iconId: string
+  title: string
+  subtitle?: string
+  backgroundColor: string
+  textColor: string
+  iconColor: string
+}
+
+const AwesomeHeader = (props: AwesomeHeaderProps) => {
+  const {
+    subtitle,
+    iconColor,
+    iconId,
+    title,
+    textColor,
+    backgroundColor,
+  } = props
   return (
-    <View>
-      <Text>AwesomeHeader</Text>
+    <View
+      style={[styles.headerContainer, { backgroundColor: backgroundColor }]}
+    >
+      <View style={styles.textContainer}>
+        <Text style={[styles.title, { color: textColor }]}>{title}</Text>
+        {subtitle && (
+          <Text style={[styles.subtitle, { color: textColor }]}>
+            {subtitle}
+          </Text>
+        )}
+      </View>
+      <View style={styles.iconContainer}>
+        <FontAwesome5 name={iconId} size={40} color={iconColor} solid />
+      </View>
     </View>
   )
 }
 
-export { AwesomeHeader }
+const styles = StyleSheet.create({
+  headerContainer: {
+    borderBottomRightRadius: 35,
+    minHeight: 80,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textContainer: {
+    justifyContent: 'center',
+    marginTop: 12,
+    marginStart: 20,
+    marginBottom: 12,
+  },
+  iconContainer: {
+    alignSelf: 'flex-end',
+    borderRadius: 50,
+    padding: 14,
+  },
+  title: {
+    fontSize: 38,
+    letterSpacing: 5,
+    fontFamily:
+      Platform.OS === 'ios' ? 'AppleSDGothicNeo-Thin' : 'sans-serif-thin',
+  },
+  subtitle: {
+    fontSize: 20,
+    fontFamily:
+      Platform.OS === 'ios' ? 'AppleSDGothicNeo-Thin' : 'sans-serif-thin',
+  },
+})
+
+export { AwesomeHeader, AwesomeHeaderProps }
